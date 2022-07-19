7c71dc4fd6c283652eaaf72ebe9ebca59820bbf6
jakeogh <github.com@v6y.net>
2022-07-18 22:20:37 -0700

    auto-commit

--- byte_vector_replacer/byte_vector_replacer.py
+++ byte_vector_replacer/byte_vector_replacer.py
@@ -148,6 +148,7 @@ def get_pairs(verbose: Union[bool, int, float]) -> dict:
         b"# pylint: disable=too-many-boolean-expressions    # [R0916] in if statement\n\n": b"# pylint: disable=too-many-boolean-expressions    # [R0916] in if statement\nfrom __future__ import annotations\n",
         b"Optional[Iterator[str]]": b"None | Iterator[str]",
         b"Optional[Iterable[str]]": b"None | Iterable[str]",
+        b"Optional[Sequence]": b"None | Sequence",
     }
     return pair_dict
 
