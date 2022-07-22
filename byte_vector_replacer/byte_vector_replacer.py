10cde032a3beec9815dd2f6709042b59a7c8ee75
jakeogh <github.com@v6y.net>
2022-07-22 13:09:36 -0700

    auto-commit

--- byte_vector_replacer/byte_vector_replacer.py
+++ byte_vector_replacer/byte_vector_replacer.py
@@ -145,7 +145,7 @@ def get_pairs(verbose: Union[bool, int, float]) -> dict:
         b"# flake8: noqa           # flake8 has no per file settings :(": None,
         b"Optional[Decimal]": b"None | Decimal",
         b"Optional[Tuple[str, ...]]": b"None | Tuple[str, ...]",
-        b"# pylint: disable=too-many-boolean-expressions    # [R0916] in if statement\n\n": b"# pylint: disable=too-many-boolean-expressions    # [R0916] in if statement\nfrom __future__ import annotations\n",
+        b"# pylint: disable=too-many-boolean-expressions    # [R0916] in if statement\n\n\n": b"# pylint: disable=too-many-boolean-expressions    # [R0916] in if statement\nfrom __future__ import annotations\n",
         b"Optional[Iterator[str]]": b"None | Iterator[str]",
         b"Optional[Iterable[str]]": b"None | Iterable[str]",
         b"Optional[Sequence]": b"None | Sequence",
