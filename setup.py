b375762595cdd1827f65af648f86d06eb9f75bc0
jakeogh <github.com@v6y.net>
2022-10-26 17:49:14 -0700

    auto-commit

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import fastentrypoints
-from setuptools import find_packages, setup
-if not sys.version_info[0] == 3:
-    sys.exit("Python 3 is required. Use: \'python3 setup.py install\'")
 
-dependencies = ["icecream", "click"]
+from setuptools import find_packages
+from setuptools import setup
+
+import fastentrypoints
+
+dependencies = ["click"]
 
 config = {
     "version": "0.1",
@@ -17,8 +18,8 @@ config = {
     "author_email": "github.com@v6y.net",
     "description": "manages and applies dict of automatically replaced byte vectors in ~/.config",
     "long_description": __doc__,
-    "packages": find_packages(exclude=['tests']),
-    "package_data": {"byte_vector_replacer": ['py.typed']},
+    "packages": find_packages(exclude=["tests"]),
+    "package_data": {"byte_vector_replacer": ["py.typed"]},
     "include_package_data": True,
     "zip_safe": False,
     "platforms": "any",
