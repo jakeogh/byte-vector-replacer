347c197f0f605685985574b1c3c55d2ddc2df714
jakeogh <github.com@v6y.net>
2024-11-16 22:15:54 -0700

    auto-commit

--- byte_vector_replacer/__init__.py
+++ byte_vector_replacer/__init__.py
@@ -1,3 +1,7 @@
-from .byte_vector_replacer import GuardFoundError
-from .byte_vector_replacer import byte_vector_replacer
-from .byte_vector_replacer import get_pairs
+"""
+isort:skip_file
+"""
+
+from .byte_vector_replacer import GuardFoundError as GuardFoundError
+from .byte_vector_replacer import byte_vector_replacer as byte_vector_replacer
+from .byte_vector_replacer import get_pairs as get_pairs
